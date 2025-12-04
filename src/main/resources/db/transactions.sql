fb149d4378cfc31b450b63f7439efe42a5b0ec55
Priit Roosmäe <priitrs@gmail.com>
2025-12-04 19:44:38 +0200

    Create transactions table

---
+++ src/main/resources/db/transactions.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset priitrs:transactions
+create table transactions (
+                           id uuid default gen_random_uuid() primary key,
+                           timestamp timestamptz not null,
+                           type text not null,
+                           quantity integer not null,
+                           price decimal not null,
+                           fee decimal not null
+);
