b5edf6b5b85c4abbedb59ff94def8b39a2b3728a
Priit Roosmäe <priitrs@gmail.com>
2025-12-08 00:02:09 +0200

    Add transaction data

--- src/main/java/ee/bank/portfolio/transactions/Transaction.java
+++ src/main/java/ee/bank/portfolio/transactions/Transaction.java
@@ -1,26 +1,31 @@
 package ee.bank.portfolio.transactions;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Instant;
 import java.util.UUID;
 
 public record Transaction(
-        UUID id,
+        @JsonIgnore UUID id,
         Instant timestamp,
         String type,
         int quantity,
         BigDecimal price,
         BigDecimal fee
 ) {
+    @JsonIgnore
     public BigDecimal getBuyTotalCost(){
        return price().multiply(BigDecimal.valueOf(quantity())).add(fee());
     }
 
+    @JsonIgnore
     public BigDecimal getBuyAverageCost(){
         return getBuyTotalCost().divide(BigDecimal.valueOf(quantity()), 6, RoundingMode.HALF_UP);
     }
 
+    @JsonIgnore
     public BigDecimal getSellProceeds(){
         return price().multiply(BigDecimal.valueOf(quantity())).subtract(fee());
     }
